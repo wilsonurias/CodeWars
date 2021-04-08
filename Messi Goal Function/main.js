d63a44aadf0f1a2a18d92e913983fbce5b28c712
Wilson Urias <wilsurias@gmail.com>
2021-04-07 21:43:27 -0400

    answer to messi goals

---
+++ Messi Goal Function/main.js
@@ -0,0 +1,9 @@
+// Messi is a soccer player with goals in three leagues:
+// LaLiga
+// Copa del Rey
+// Champions League
+// Complete the function to return his total number of goals in all three leagues.
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals  
+}
