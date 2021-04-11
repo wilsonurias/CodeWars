a9ebcbc6db9be65a1b951fd9331fb28da97682ce
Wilson Urias <wilsurias@gmail.com>
2021-04-11 13:51:46 -0400

    debugged code

---
+++ Debug SayHello/main.js
@@ -0,0 +1,12 @@
+//  It is your job to fix the code and get the program working again!
+
+// Bugged code:
+// function sayHello(name) {
+//   return 'Hello'
+// }
+
+// Debugged Code:
+
+function sayHello(name) {
+  return `Hello, ${name}`
+}
