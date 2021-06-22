3141ab27f941ab538b947c56ac957bdb774f508e
Wilson Urias <wilsurias@gmail.com>
2021-06-22 19:04:43 -0400

    answer to drink about it

---
+++ Drink About It/main.js
@@ -0,0 +1,15 @@
+// Make a function that receive age, and return what they drink.
+
+
+function peopleWithAgeDrink(old) {
+    if (old <14){
+      return 'drink toddy'
+    }else if(old <18){
+      return 'drink coke'
+    }else if(old < 21){
+      return 'drink beer'
+    }else {
+      return 'drink whisky'
+    }
+    
+} 
