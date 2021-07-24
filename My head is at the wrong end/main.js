dea1fdce59580dd5eff93411f1ebbeddadfa4fd2
Wilson Urias <wilsurias@gmail.com>
2021-07-23 20:41:12 -0400

    reversing an array

---
+++ My head is at the wrong end/main.js
@@ -0,0 +1,8 @@
+// You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+
+function fixTheMeerkat(arr) {
+    let tailBodyHead = arr
+    return tailBodyHead.reverse()
+}
