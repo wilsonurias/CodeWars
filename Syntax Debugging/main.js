ece5b528d7a26fe5152aeb7ab999c953868dbd8f
Wilson Urias <wilsurias@gmail.com>
2021-08-19 22:04:42 -0400

    debugging syntax

---
+++ Syntax Debugging/main.js
@@ -0,0 +1,14 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// bugged code
+
+function main [verb, noun]
+  return verb + noun
+}
+
+
+// debugged code
+
+function main(verb, noun) {
+    return verb + noun
+}
